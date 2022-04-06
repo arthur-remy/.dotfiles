e01ccfe73fa123df488dc997fcc8be27c9091375
Arthur <arthur.simon.remy@gmail.com>
2022-04-06 11:45:43 +0200

    minor adjustments

--- aliases.sh
+++ aliases.sh
@@ -3,17 +3,17 @@
 # CHANGE DEFAULT BEHAVIOR OF DEFAULT CMD
 
 alias ls='ls -lah --color=auto'
-alias grep='grep --colour=auto'
+alias grep='grep --color=auto'
 alias mv='mv -i'
 alias cp='cp -i'
 alias ln='ln -i'
 
-export LIBVIRT_DEFAULT_URI='qemu:///system'  # connect to root's vms
+export LIBVIRT_DEFAULT_URI='qemu:///system'  # connect to root's VMs
 export EDITOR=vim
 
 # CUSTOM COMMANDS
 
-alias sshpi='ssh user@pi.local'
+alias sshpi='ssh pi@pi.local'
 alias helpcmd='vim $HOME/.dotfiles/frequent_cmd.txt'
 alias todo='vim $HOME/Documents/Notes/todo.md'
 
